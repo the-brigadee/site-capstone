ee256b6250502f56a46780e0224693eba65d0893
brightodedo <54002497+brightodedo@users.noreply.github.com>
2022-07-18 13:24:40 -0400

    bare bone setup for the ui

---
+++ capstone-ui/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import './Navbar.css'
+
+
+export default function Navbar(){
+    return(
+        <nav className='navbar'>
+            <div className="logo-container">
+                <img src="https://cdn.iconscout.com/icon/free/png-256/chef-1828025-1551570.png" alt="Logo img" className='logo' />
+            </div>
+
+        </nav>
+    )
+}
